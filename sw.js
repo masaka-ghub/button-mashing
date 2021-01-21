7ace88b5e4d7a1740e12ef79a2f48ff17e681abe
masahiro.kawakami <masahiro.kawakami@rakus.co.jp>
2021-01-21 12:20:03 +0900

    workboxによるコンテンツのプリキャッシュ

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+
+workbox.precaching.precacheAndRoute([
+  '/index.html',
+  '/app.css',
+  '/app.js'
+]);
+
 self.addEventListener('fetch', (e) => {})
